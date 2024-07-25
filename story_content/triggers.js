c8dfb7fe0b2990c5a0e8984b548dc0d3b2c1d6c2
Mohamed <mohammad.elmedawar@gmail.com>
2024-07-25 10:58:59 +0300

    Interactive Lesson

---
+++ story_content/triggers.js
@@ -0,0 +1,78 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6cCHc4f7o0l":
+        Script1();
+        break;
+      case "5p4WfjeKPgF":
+        Script2();
+        break;
+      case "5i0Eg3zOWMu":
+        Script3();
+        break;
+      case "6XlGXWXRhHt":
+        Script4();
+        break;
+      case "6TXRhb7QuiH":
+        Script5();
+        break;
+      case "61j1G9RmddF":
+        Script6();
+        break;
+      case "5kJqvZLEVI9":
+        Script7();
+        break;
+      case "5WVIdWOCO4Y":
+        Script8();
+        break;
+      case "6RGlwYAANyK":
+        Script9();
+        break;
+      case "6fLHaOoQ2vZ":
+        Script10();
+        break;
+      case "6cbJ2xX6Zuf":
+        Script11();
+        break;
+      case "5aXShFRJ7DE":
+        Script12();
+        break;
+      case "6eLnZmFhYX0":
+        Script13();
+        break;
+      case "6fCtMAy8GhN":
+        Script14();
+        break;
+      case "6Y9MKBsELiO":
+        Script15();
+        break;
+      case "6kKoOGBY9kD":
+        Script16();
+        break;
+      case "61jkU2UD72R":
+        Script17();
+        break;
+      case "5oqszVSCVvG":
+        Script18();
+        break;
+      case "5gpn4K3qh58":
+        Script19();
+        break;
+      case "6fbEjt6nfnX":
+        Script20();
+        break;
+      case "5YjyCs356PC":
+        Script21();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
